20700f5208775b2c1b98722bffb448fd4f59c874
TSUCCI-412 <82869219+TSUCCI-412@users.noreply.github.com>
2021-09-25 00:04:24 +0900

    js追加

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(function() {
+    $('.c-toggle').on("click", function(){
+  
+      $(this).toggleClass('active');
+      $('.p-gnav').toggleClass('active');
+    });
+     console.log ("成功");
+  });
+
+  
+
+  
+
+
+
+
