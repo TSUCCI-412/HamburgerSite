d1907ed5ae2cf83373040e0a2e2be2cbcf6073a2
TSUCCI-412 <82869219+TSUCCI-412@users.noreply.github.com>
2021-10-03 20:31:48 +0900

    scriptのチェック

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 $(function() {
-    $('.c-toggle').on("click", function(){
+  $(".p-gnav__list").hide();
+  $(".c-toggle").on("click",function(){//メニューボタンをクリックしたとき
+    $(".p-gnav__list").toggle(300);//0.3秒で表示したり非表示にしたりする
+    $(".c-toggle--line").toggleClass("active");
+ 
+
+});
+    
   
-      $(this).toggleClass('active');
-      $('.p-gnav').toggleClass('active');
-    });
      console.log ("成功");
   });
 
