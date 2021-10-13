79a9752cd512fc70a1f9dcb5f9ecf9d7c8b0a019
TSUCCI-412 <82869219+TSUCCI-412@users.noreply.github.com>
2021-10-13 09:12:41 +0900

    コード確認

--- js/script.js
+++ js/script.js
@@ -1,15 +1,27 @@
 $(function() {
-  $(".p-gnav__list").hide();
   $(".c-toggle").on("click",function(){//メニューボタンをクリックしたとき
-    $(".p-gnav__list").toggle(300);//0.3秒で表示したり非表示にしたりする
-    $(".c-toggle--line").toggleClass("active");
- 
+    //$(".p-gnav__list").toggle(400);//0.4秒で表示したり非表示にしたりする
+    $(".is-open").toggleClass("active"); //Menuと✖の切り替え
+    $(".is-fade").toggleClass("cover");
+    if($('.p-gnav').hasClass('is-menu')){　　//横からメニュースライド
+      $('.p-gnav').removeClass('is-menu');
+      $(".p-gnav").animate({'marginRight':'300px'},300).addClass('on-menu');
+    }else{
+      $('.p-gnav').addClass('is-menu');
+      $(".p-gnav").animate({'marginRight':'0px'},300);
+    };
+
+
+   
 
 });
     
   
      console.log ("成功");
   });
+
+
+  
 
   
 
