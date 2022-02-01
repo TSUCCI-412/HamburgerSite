675a7c4e17cc3c054df0df45c06f488f2737a084
TSUCCI-412 <82869219+TSUCCI-412@users.noreply.github.com>
2022-02-01 12:59:49 +0900

    変更

--- js/script.js
+++ js/script.js
@@ -1,27 +1,32 @@
 $(function() {
-  //読み込み時とブラウザサイズを変更した時に処理
+//読み込み時とブラウザサイズを変更した時に処理
   $(window).on('load resize', function(){
-    //ウィンドウサイズを取得する
+//ウィンドウサイズを取得する
     var w = $(window).width();
-    //ウィンドウサイズが1024より小さい場合
+//ウィンドウサイズが1024より小さい場合
     if (window.matchMedia( "(max-width: 1024px)" ).matches) {
 
-      $(".c-toggle").on("click",function() {
+      $(".c-btn--sp").on("click",function() {
         $(".is-open").toggleClass("active");  
         $('.is-menu').toggleClass('on-menu');
         $('.is-fade').toggleClass("cover");
       
       })  
-     //ウィンドウサイズが1025より大きい場合 
-    }else{ (window.matchMedia( "(min-width: 1024px)" ).matches) 
-      
-      $(".c-toggle").on("click",function() {
+
+      $(".c-toggle--line").on("click",function() {
         $(".is-open").removeClass("active");  
         $('.is-menu').removeClass('on-menu');
+        $('.is-fade').removeClass("cover");
+      
       })
-    }  
+
+    } else{ (window.matchMedia( "(min-width: 1024px)" ).matches) 
+        $(".is-open").removeClass("active"); 
+        $('.is-menu').removeClass('on-menu');
+        $('.is-fade').removeClass("cover"); 
+    }    
   });
 });
 
 
-
+ 
