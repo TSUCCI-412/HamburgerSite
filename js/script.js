843b485947ec0dcc093da2b40cf4b2711f76be8b
TSUCCI-412 <82869219+TSUCCI-412@users.noreply.github.com>
2022-02-07 00:07:22 +0900

    コード確認

--- js/script.js
+++ js/script.js
@@ -1,30 +1,57 @@
-$(function() {
-  //読み込み時とブラウザサイズを変更した時に処理
-    $(window).on('load resize', function(){
-  //ウィンドウサイズを取得する
-      var w = $(window).width();
-  //ウィンドウサイズが1024より小さい場合
-      if (window.matchMedia( "(max-width: 1024px)" ).matches) {
-  
-        $(".c-btn--sp").on("click",function() {
-          $(".is-open").toggleClass("active");  
-          $('.is-menu').animate({'marginRight':'300px'},400);
-          $('.is-fade').toggleClass("cover");
-        
-        })  
-  
-        $(".c-toggle--line").on("click",function() {
-          $(".is-open").removeClass("active");  
-          $('.is-menu').animate({'marginRight':'-300px'},400);
-          $('.is-fade').removeClass("cover");
-        
-        })
-  
-      } else{ (window.matchMedia( "(min-width: 1025px)" ).matches)
-          $(".is-open").removeClass("active"); 
-          $('.is-fade').removeClass("cover");
-      
-      }    
+$(function(){
+
+  $('.c-btn--sp').on('click',function(){
+    //1024px以下なら
+    if (window.matchMedia( "(max-width: 1024px)" ).matches) {
+    
+      $(".is-open").addClass("active");  
+      $('.is-menu').addClass('on-menu');
+      $('.is-fade').addClass("cover");
+
+    }
+  })
+
+  $(".c-toggle--line").on("click",function() {
+    if (window.matchMedia( "(max-width: 1024px)" ).matches) {
+   
+    $(".is-open").toggleClass("active");  
+    $('.is-menu').toggleClass('on-menu');
+    $('.is-fade').toggleClass("cover");
+    }
+  })
+
+  $(document).ready(function() {
+    var checkResize;
+    $(window).on('load resize', function() {
+        clearTimeout( checkResize );
+        checkResize = setTimeout( resizing, 100 );
     });
-  });
   
+    //リサイズ完了時に実行する処理
+    function resizing() {
+      var w = $(window).width();
+  
+      //ウィンドウサイズが1023より大きい場合の処理
+      if( w > 1023 ) {
+
+        $('.is-open').hasClass('active');
+        $('.is-open').removeClass('active');
+
+        $('.is-fade').hasClass('cover');
+        $('.is-fade').removeClass('cover');
+
+        $('.is-menu').hasClass('on-menu');
+        $('.is-menu').removeClass('on-menu');
+
+        
+  
+      //ウィンドウサイズが1025以下の場合の処理
+      } else {
+        console.log('ウィンドウサイズは1025以下です。');
+      }
+    }
+  });
+
+}); //最後のカッコ
+
+
